792dfe492343ae42fb3396505628576284387e79
Jean Dobler <jedobler@gmail.com>
2019-02-24 22:45:35 -0300

    Nova tela, Adicionado material Design, iniciando tela  de challenge

---
+++ app/src/main/java/com/dynamic/dobler/elephantmath/activity/Challenge.java
@@ -0,0 +1,16 @@
+package com.dynamic.dobler.elephantmath.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dynamic.dobler.elephantmath.R;
+
+public class Challenge extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge);
+    }
+}
--- app/src/main/java/com/dynamic/dobler/elephantmath/activity/MainActivity.java
+++ app/src/main/java/com/dynamic/dobler/elephantmath/activity/MainActivity.java
@@ -1,27 +1,41 @@
 package com.dynamic.dobler.elephantmath.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 import com.dynamic.dobler.elephantmath.R;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.dynamic.dobler.elephantmath.activity.ranking.ItemListActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.cv_main_ranking)
+    CardView mCvRanking;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mCvRanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRanking();
+            }
+        });
 
+    }
 
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
-
+    protected void openRanking() {
+        Intent rankingIntent = new Intent(this, ItemListActivity.class);
+        startActivity(rankingIntent);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
